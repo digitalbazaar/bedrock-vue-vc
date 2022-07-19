190f97df03dfa0590a6ccc3658ec86d48758711c
Tyler Olson <tolson@dlinc.io>
2022-07-19 12:24:30 -0400

    Update and fix tests.

--- test/web/10-CredentialSwitch.js
+++ test/web/10-CredentialSwitch.js
@@ -1,122 +1,77 @@
 /*!
  * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
  */
-import {createApp} from 'vue';
-import {CredentialSwitch} from '@bedrock/vue-vc';
+import AlumniDisplay from './../components/examples/alumniDisplay.vue';
+import {alumniCredential, basicCredential} from './mock-credentials.js';
+import {config} from '@bedrock/web';
+import {CredentialSwitch, registerComponent} from '@bedrock/vue-vc';
+import {default as iconSet} from 'quasar/icon-set/fontawesome-v5.js';
+import Quasar from 'quasar';
+import Vue from 'vue'
 
-// helper function that mounts and returns the rendered text
-function renderCredential(propsData) {
-  const app = createApp(CredentialSwitch, propsData);
-  // use the universal selector to get the first element
-  return app.mount('*');
+
+function tearDown(app) {
+  return app.unmount();
 }
 
-describe('CredentialCard', () => {
-  it('should render content if type of value is a string in credential ' +
-    'subject', async () => {
-    const vm = renderCredential({
-      credential: {
-        credentialSubject: {
-          name: 'John Doe',
-        },
-        type: ['TestCredential'],
-        description: 'Test description',
-        image: 'http://example.com/some-image.png'
-      },
-      schema: {
-        name: {
-          name: 'Full Name',
-          icon: 'fas fa-user'
-        }
-      }
-    });
-    console.log({vm});
+function createApp({props}) {
+  const app = Vue.createApp(CredentialSwitch, props);
+  app.use(Quasar);
+  Quasar.iconSet.set(iconSet);
+  return app;
+}
+
+// helper function that mounts and returns the rendered text
+function renderCredential({app = null, props = {}}) {
+  if(!app) {
+    app = createApp({props});
+  }
+  const vm = app.mount('*');
+  return {app, vm};
+}
+
+describe('CredentialSwitch', () => {
+  it('should render basic credential in list mode', async () => {
+    const {app, vm} = await renderCredential({props: {credential: basicCredential}});
     should.exist(vm);
     should.exist(vm.$el);
-    vm.$el.querySelector('.g-field-data-regular')
-      .textContent.trim().should.equal('John Doe');
+    vm.$el.querySelector('.cf-title').textContent
+      .trim().should.equal('Test Credential');
+    vm.$el.querySelector('.cf-value').textContent
+      .trim().should.equal(basicCredential.description);
+    tearDown(app)
   });
 
-  it('should not render content if type of value in credential ' +
-    'subject is null or undefined', async () => {
-    const valueTypes = [undefined, null];
-
-    for(const value of valueTypes) {
-      const vm = renderCredential({
-        credential: {
-          credentialSubject: {
-            name: value,
-          },
-          type: ['TestCredential'],
-          description: 'Test description',
-          image: 'http://example.com/some-image.png'
-        },
-        schema: {
-          name: {
-            name: 'Full Name',
-            icon: 'fas fa-user'
-          }
-        }
-      });
-      should.exist(vm);
-      should.exist(vm.$el);
-      should.not.exist(vm.$el.querySelector('.g-field-data-regular'));
-    }
-  });
-
-  it('should render content if type of value in credential ' +
-    'subject is a number, NaN, or boolean', async () => {
-    const valueTypes = [ NaN, 0, false];
-
-    for(const value of valueTypes) {
-      const vm = renderCredential({
-        credential: {
-          credentialSubject: {
-            name: value,
-          },
-          type: ['TestCredential'],
-          description: 'Test description',
-          image: 'http://example.com/some-image.png'
-        },
-        schema: {
-          name: {
-            name: 'Full Name',
-            icon: 'fas fa-user'
-          }
-        }
-      });
-      should.exist(vm);
-      should.exist(vm.$el);
-      should.exist(vm.$el.querySelector('.g-field-data-regular'));
-    }
-  });
-  it('should render content if type of value in credential ' +
-    'subject is an object', async () => {
-    const vm = renderCredential({
-      credential: {
-        credentialSubject: {
-          address: {
-            streetAddress: '123 Main St.',
-            addressCountry: 'US',
-            addressLocality: 'Chicago',
-            addressRegion: 'IL',
-            postalCode: '60610-2949'
-          }
-        },
-        type: ['TestCredential'],
-        description: 'Test description',
-        image: 'http://example.com/some-image.png'
-      },
-      schema: {
-        address: {
-          name: 'Address',
-          icon: 'fa fa-map-marker-alt',
-          sublabels: true
-        }
-      }
-    });
+  it('should render basic credential in details mode', async () => {
+    const {vm, app} = await renderCredential({props: {
+      credential: basicCredential, mode: 'details'}});
     should.exist(vm);
     should.exist(vm.$el);
-    should.exist(vm.$el.querySelector('.g-field-data-regular'));
+    vm.$el.querySelector('.cf-title').textContent
+      .trim().should.equal('Test Credential');
+    vm.$el.querySelector('.cf-value').textContent
+      .trim().should.equal(basicCredential.description);
+    tearDown(app)
+  });
+
+  it('should render credential for registered component', async () => {
+    const app = await createApp({props: {credential: alumniCredential, mode: 'details'}});
+    await registerComponent({app, component: AlumniDisplay, name: 'AlumniDisplay'});
+    config.credentialDisplay = {
+      registration: [{
+        acceptableTypes: ['AlumniCredential'],
+        components: {
+          details: [{component: 'AlumniDisplay'}],
+        }
+      }]
+    };
+    const {vm} = renderCredential({app})
+    should.exist(vm);
+    should.exist(vm.$el);
+    vm.$el.querySelector('.cf-title').textContent
+      .trim().should.equal('Granted by');
+    vm.$el.querySelector('.cf-value').textContent
+      .trim().should.equal(alumniCredential.credentialSubject.alumniOf.name[0].value);
+    tearDown(app)
   });
 });
