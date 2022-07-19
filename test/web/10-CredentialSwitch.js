33f31795f73c9837abd61cc6ee56ef6430e2a86a
Tyler Olson <tolson@dlinc.io>
2022-07-19 12:34:04 -0400

    Lint.

--- test/web/10-CredentialSwitch.js
+++ test/web/10-CredentialSwitch.js
@@ -1,30 +1,29 @@
 /*!
  * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
  */
-import AlumniDisplay from './../components/examples/alumniDisplay.vue';
 import {alumniCredential, basicCredential} from './mock-credentials.js';
-import {config} from '@bedrock/web';
 import {CredentialSwitch, registerComponent} from '@bedrock/vue-vc';
+import AlumniDisplay from './../components/examples/alumniDisplay.vue';
+import {config} from '@bedrock/web';
 import {default as iconSet} from 'quasar/icon-set/fontawesome-v5.js';
 import Quasar from 'quasar';
-import Vue from 'vue'
-
+import Vue from 'vue';
 
 function tearDown(app) {
   return app.unmount();
 }
 
-function createApp({props}) {
-  const app = Vue.createApp(CredentialSwitch, props);
+function createApp({propsData = {}}) {
+  const app = Vue.createApp(CredentialSwitch, propsData);
   app.use(Quasar);
   Quasar.iconSet.set(iconSet);
   return app;
 }
 
 // helper function that mounts and returns the rendered text
-function renderCredential({app = null, props = {}}) {
+function renderCredential({app = null, propsData = {}}) {
   if(!app) {
-    app = createApp({props});
+    app = createApp({propsData});
   }
   const vm = app.mount('*');
   return {app, vm};
@@ -32,18 +31,19 @@ function renderCredential({app = null, props = {}}) {
 
 describe('CredentialSwitch', () => {
   it('should render basic credential in list mode', async () => {
-    const {app, vm} = await renderCredential({props: {credential: basicCredential}});
+    const {app, vm} = await renderCredential({
+      propsData: {credential: basicCredential}});
     should.exist(vm);
     should.exist(vm.$el);
     vm.$el.querySelector('.cf-title').textContent
       .trim().should.equal('Test Credential');
     vm.$el.querySelector('.cf-value').textContent
       .trim().should.equal(basicCredential.description);
-    tearDown(app)
+    tearDown(app);
   });
 
   it('should render basic credential in details mode', async () => {
-    const {vm, app} = await renderCredential({props: {
+    const {vm, app} = await renderCredential({propsData: {
       credential: basicCredential, mode: 'details'}});
     should.exist(vm);
     should.exist(vm.$el);
@@ -51,12 +51,14 @@ describe('CredentialSwitch', () => {
       .trim().should.equal('Test Credential');
     vm.$el.querySelector('.cf-value').textContent
       .trim().should.equal(basicCredential.description);
-    tearDown(app)
+    tearDown(app);
   });
 
   it('should render credential for registered component', async () => {
-    const app = await createApp({props: {credential: alumniCredential, mode: 'details'}});
-    await registerComponent({app, component: AlumniDisplay, name: 'AlumniDisplay'});
+    const app = await createApp({
+      propsData: {credential: alumniCredential, mode: 'details'}});
+    await registerComponent({
+      app, component: AlumniDisplay, name: 'AlumniDisplay'});
     config.credentialDisplay = {
       registration: [{
         acceptableTypes: ['AlumniCredential'],
@@ -65,13 +67,14 @@ describe('CredentialSwitch', () => {
         }
       }]
     };
-    const {vm} = renderCredential({app})
+    const {vm} = renderCredential({app});
     should.exist(vm);
     should.exist(vm.$el);
     vm.$el.querySelector('.cf-title').textContent
       .trim().should.equal('Granted by');
     vm.$el.querySelector('.cf-value').textContent
-      .trim().should.equal(alumniCredential.credentialSubject.alumniOf.name[0].value);
-    tearDown(app)
+      .trim().should.equal(
+        alumniCredential.credentialSubject.alumniOf.name[0].value);
+    tearDown(app);
   });
 });
--- test/web/mock-credentials.js
+++ test/web/mock-credentials.js
@@ -1,44 +1,44 @@
 export const alumniCredential = {
   '@context': [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://www.w3.org/2018/credentials/examples/v1"
+    'https://www.w3.org/2018/credentials/v1',
+    'https://www.w3.org/2018/credentials/examples/v1'
   ],
-  id: "http://example.edu/credentials/58473",
+  id: 'http://example.edu/credentials/58473',
   type: [
-    "VerifiableCredential",
-    "AlumniCredential"
+    'VerifiableCredential',
+    'AlumniCredential'
   ],
   issuer: {
-    id: "did:example:12345678910111213",
-    image: "http://example.com/some-image.png"
+    id: 'did:example:12345678910111213',
+    image: 'http://example.com/some-image.png'
   },
-  name: "Alumni Credential",
-  description: "The holder is an alumni of Example University.",
-  issuanceDate: "2018-05-14T12:00:00Z",
+  name: 'Alumni Credential',
+  description: 'The holder is an alumni of Example University.',
+  issuanceDate: '2018-05-14T12:00:00Z',
   credentialSubject: {
-    id: "did:example:ebfeb1f712ebc6f1c276e12ec21",
+    id: 'did:example:ebfeb1f712ebc6f1c276e12ec21',
     alumniOf: {
-      id: "did:example:c276e12ec21ebfeb1f712ebc6f1",
+      id: 'did:example:c276e12ec21ebfeb1f712ebc6f1',
       name: [
         {
-          value: "Example University",
-          lang: "en"
+          value: 'Example University',
+          lang: 'en'
         },
         {
-          value: "Exemple d'Université",
-          lang: "fr"
+          value: 'Exemple d\'Université',
+          lang: 'fr'
         }
       ]
     }
   },
   proof: {
-    type: "Ed25519Signature2020",
-    created: "2021-11-13T18:19:39Z",
-    verificationMethod: "https://example.edu/issuers/14#key-1",
-    proofPurpose: "assertionMethod",
-    proofValue: "z58DAdFfa9SkqZMVPxAQpic7ndSayn1PzZs6ZjWp1CktyGesjuTSwRdoWhAfGFCF5bppETSTojQCrfFPP2oumHKtz"
+    type: 'Ed25519Signature2020',
+    created: '2021-11-13T18:19:39Z',
+    verificationMethod: 'https://example.edu/issuers/14#key-1',
+    proofPurpose: 'assertionMethod',
+    proofValue: 'z58DAdFfa9SkqZMVPxAQpic7ndSaynfFPP2oumHKtz'
   }
-}
+};
 
 export const basicCredential = {
   credentialSubject: {
